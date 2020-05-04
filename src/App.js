e8a60c46913b8b23312ee6828515cc45b5de78de
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-05-04 20:46:31 +0200

    Get the data in effect in App instead of index

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DataTable from './DataTable';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container mt-3">
-        <DataTable rows={this.props.rows} locale="da" rowsPerPage={5} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(document.getElementById('user-data').dataset.users);
+    setUserData(data);
+  }, []);
+
+  return (
+    <div className='container mt-3'>
+      <DataTable rows={userData} locale='da' rowsPerPage={5} />
+    </div>
+  );
+};
 
 export default App;
